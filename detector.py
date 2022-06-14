1103de75512e8d28145655884f62e251eff179bd
Yehuda Rosenberg <79972077+YehudaRosenberg@users.noreply.github.com>
2022-06-14 14:57:07 +0300

    attackers mac address + corrupted interface

--- detector.py
+++ detector.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-#@TODO: create a function to get the interface, create windows program with tkinter
-#@TODO: print the MAC number and IP of the atacker
+#TODO 1: Rebuild the code with OOP 
+#TODO 2: Create windows program with tkinter (maybe use PyScript?)
+#TODO 3: Think about adding a feature like sending email to the corrupted netowrk owner (smtlib etc.) 
 
 import scapy.all as scapy
+
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -12,18 +14,19 @@ def get_mac(ip):
 
     return answered_list[0][1].hwsrc
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+def sniff():
+    scapy.sniff(store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
     if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
         try:
             real_mac = get_mac(packet[scapy.ARP].psrc)
             response_mac = packet[scapy.ARP].hwsrc
-
+            corrupted_iface = scapy.conf.iface
             if real_mac != response_mac:
-                print("[+] You are under attack!!")
+                attack_msg = f"[+] You are under attack!! \nAttacker's MAC Address is {response_mac}. \nThe Corrupted interface is {corrupted_iface}"
+                print(attack_msg)
         except IndexError:
             pass
 
-sniff("eth0")
+sniff()
